451b8d94e0b0a3662dd19fe7950c69514fd993d6
Yjuser1 <yj.mail520@gmail.com>
2022-11-12 15:08:55 +0800

    Site updated: 2022-11-12 15:08:51

--- js/diytitle.js
+++ js/diytitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = 'w(ﾟДﾟ)w 不要走！再看看嘛！';
+        document.title = '🐱‍👓找不到我喽！';
         clearTimeout(titleTime);
     }
     else {
         //返回当前页面时标签显示内容
-        document.title = '♪(^∇^*)欢迎回来！' + OriginTitile;
+        document.title = '💡被你发现了！' + OriginTitile;
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
