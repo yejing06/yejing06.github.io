3b69e6cc655ed7c3b6e26a12ee74168422945d27
yejing06 <102644517+yejing06@users.noreply.github.com>
2023-03-17 02:39:07 +0000

    github action update Updated By Github Actions

--- js/diytitle.js
+++ js/diytitle.js
@@ -1,18 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        //离开当前页面时标签显示内容
-        document.title = '为人民谋幸福！';
-        clearTimeout(titleTime);
-    }
-    else {
-        //返回当前页面时标签显示内容
-        document.title = '为民族谋复兴！' + OriginTitile;
-        //两秒后变回正常标题
-        titleTime = setTimeout(function () {
-            document.title = OriginTitile;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="为人民谋幸福！",clearTimeout(titleTime)):(document.title="为民族谋复兴！"+OriginTitile,titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
